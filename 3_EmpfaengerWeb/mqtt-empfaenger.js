0405e1ed37b282f693110b7c037d07c2e96ff634
MDecker-MobileComputing <mide_42b@yahoo.de>
2025-03-18 16:17:19 +0100

    Web-Empfaenger: RadioButtons dazu

--- 3_EmpfaengerWeb/mqtt-empfaenger.js
+++ 3_EmpfaengerWeb/mqtt-empfaenger.js
@@ -1,7 +1,17 @@
 "use strict";
 
+/** Topic für Inlands-Schlagzeilen. */
+const topicInland  = "dozent/decker/nachrichten/inland";
+
+/** Topic für Auslands-Schlagzeilen. */
+const topicAusland = "dozent/decker/nachrichten/ausland";
+
+
 /** <div>-Element, unter dem die <p>-Elemente */
 let divNachrichten = null;
+
+/** MQTT-Client für Empfang von Nachrichten. */
+let mqttClient = null;
 
 
 /**
@@ -12,20 +22,22 @@ window.addEventListener( "load", async function () {
 
     divNachrichten = document.getElementById( "nachrichten" );
 
-    await mqttKanalAbonnieren();
+    const empfaengerOption = document.getElementById( "empfangsoptionen" );
+    empfaengerOption.addEventListener( "change", onRadioButtonChanged );
+
+    await mqttClientErzeugen();
 
     console.log( "Initialisierung abgeschlossen." );
 });
 
 
 /**
- * MQTT-Abonnement für die kanäle "dozent/decker/nachrichten/#".
+ * MQTT-Client erzeugen, aber ohne Abonnement.
  */
-async function mqttKanalAbonnieren() {
+async function mqttClientErzeugen() {
 
     const randomHex = Math.random().toString( 16 ).slice( 2, 10 );
     const clientId = "badnews-webclient-" + randomHex; // Beispiel: badnews-webclient-8d095061
-    console.log( "Client-ID: " + clientId );
 
     const konfigObjekt = {
         clientId: clientId,
@@ -34,12 +46,12 @@ async function mqttKanalAbonnieren() {
         clean   : true
     };
 
-    const mqttClient = await mqtt.connectAsync( mqttUrl, konfigObjekt );
-
-    await mqttClient.subscribeAsync( "dozent/decker/nachrichten/#" );
+    mqttClient = await mqtt.connectAsync( mqttUrl, konfigObjekt );
 
     mqttClient.on( "message", mqttNachrichtEmpfangen );
     mqttClient.on( "error"  , mqttFehlerAufgetreten  );
+
+    console.log( `MQTT-Verbindung mit ClientID=${clientId} aufgebaut.` );
 }
 
 
@@ -76,3 +88,72 @@ function mqttFehlerAufgetreten( fehler ) {
 
     console.error( "MQTT-Fehler aufgetreten: " + fehler );
 }
+
+
+/**
+ * Event-Handler-Funktion für Änderungen am Radio-Button.
+ *
+ * @param {*} event Objekt mit Details zum Ereignis
+ */
+async function onRadioButtonChanged( event ) {
+
+    const neueOption = event.target.value;
+
+    console.log( "Neue Empfangsoption: " + neueOption );
+
+    switch ( neueOption ) {
+
+        case "inland":
+            await subscriptionAktualisieren( true, false );
+            break;
+
+        case "ausland":
+            await subscriptionAktualisieren( false, true );
+            break;
+
+        case "beide":
+            await subscriptionAktualisieren( true, true );
+            break;
+
+        case "keine":
+            await subscriptionAktualisieren( false, false );
+            break;
+
+        default:
+            console.log( `Ungültige Empfangsoption: ${neueOption}` );
+    }
+};
+
+
+/**
+ * Aktualisiert die Abonnements für Inlands- und Auslandsnachrichten.
+ *
+ * @param {*} inlandAktiv `true`, wenn Inlandsnachrichten empfangen werden sollen
+ *
+ * @param {*} auslandAktiv `true`, wenn Auslandsnachrichten empfangen werden sollen
+ */
+async function subscriptionAktualisieren( inlandAktiv, auslandAktiv ) {
+
+    try {
+
+        // Tabula Rasa
+        await mqttClient.unsubscribeAsync( topicInland  );
+        await mqttClient.unsubscribeAsync( topicAusland );
+        console.log( "Alle Abonnements gelöscht." );
+
+        if ( inlandAktiv ) {
+
+            await mqttClient.subscribeAsync( topicInland );
+            console.log( "Abonnement für Inlandsnachrichten erstellt." );
+        }
+        if ( auslandAktiv ) {
+
+            await mqttClient.subscribeAsync( topicAusland );
+            console.log( "Abonnement für Auslandsnachrichten erstellt." );
+        }
+    }
+    catch ( fehler ) {
+
+        console.error( "Fehler beim Aktualisieren der Abonnements: " + fehler );
+    }
+};
